0fa5f6bc40b35eb5170ed103b281b48cf223170b
Rafael9771 <rafael.ramosmoreno97@gmail.com>
2019-11-25 11:02:23 -0600

    Agregando configuracion de documentos estaticos

--- ProyectoFinal/ProyectoFinal/settings.py
+++ ProyectoFinal/ProyectoFinal/settings.py
@@ -108,6 +108,10 @@ CACHES = {
     }
 }
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'statics'),)
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
